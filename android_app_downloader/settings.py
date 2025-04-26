f3a44518a0cf4ded270b35df840c5b1e5500e415
veena <veenavenuk@gmailcom>
2025-04-27 02:04:19 +0530

    Default Database URL-1

--- android_app_downloader/settings.py
+++ android_app_downloader/settings.py
@@ -166,13 +166,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://postgres:root@localhost:5432/db',
-        conn_max_age=600
-    )
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600)
+#     'default': dj_database_url.config(
+#         default='postgres://postgres:root@localhost:5432/db',
+#         conn_max_age=600
+#     )
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
